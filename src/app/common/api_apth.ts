d35f06e6a418676d5d49624c97b206e3af1b0a6c
huphc1023 <phuoclh@vnext.com.vn>
2020-03-27 13:52:28 +0700

    aupdate

--- src/app/common/api_apth.ts
+++ src/app/common/api_apth.ts
@@ -3,7 +3,7 @@ import { ApplicationProperties } from './application.properties';
 export class PathAPI {
 
     // get categories
-    // public static GET_CATEGORIES = 'https://ces-testing.azurewebsites.net/category/get-sub-category-by-id?categoryId=tikiC1_2549&fbclid=IwAR2x6dzii7j1Lk-NGBc3HtpdygaRx62KTTpra2WYb2rf-ifg4ZPLz_iqKQc';
-    public static GET_CATEGORIES = 'https://ces-testing.azurewebsites.net/category/get-sub-category-by-id';
-    public static GET_SHOP = 'https://shopee.vn/api/v2/content/official_shop?catid=84';
+    // public static GET_CATEGORIES = ApplicationProperties.PATH_API.concat('api/v1/Category/GetCategoryById');
+    public static GET_CATEGORIES = ApplicationProperties.PATH_API.concat('category/get-category-by-id');
+    public static GET_SHOP = ApplicationProperties.PATH_API.concat('api/v1/Category/GetShopById');
 }
--- src/app/common/application.properties.ts
+++ src/app/common/application.properties.ts
@@ -1,5 +1,6 @@
 export class ApplicationProperties {
     public static RECORD_IN_PAGE = 20;
+    public static APPLICATION_ID = '616b5e77-2162-4d8c-91e8-cb28251e21f8';
 
-    public static PATH_API = 'http://localhost:8081/';
+    public static PATH_API = 'https://ces-testing.azurewebsites.net/';
 }
