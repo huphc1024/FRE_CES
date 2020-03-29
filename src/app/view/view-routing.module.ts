5e1c455b88b0fc4d6d826c3ce90243e47a070ea7
QuyenLH <quyenlh@vnext.com.vn>
2020-03-30 00:54:32 +0700

    fix loi source mau do

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsComponent } from './forms/forms.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { TablesComponent } from './tables/tables.component';
-import { IconsComponent } from './icons/icons.component';
-import { TypographyComponent } from './typography/typography.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AccordionsComponent } from './accordions/accordions.component';
-import { BadgesComponent } from './badges/badges.component';
-import { ProgressbarComponent } from './progressbar/progressbar.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
-import { TooltipsComponent } from './tooltips/tooltips.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { CategoryComponent } from './view/category/category.component';
-import { CategoryDetailComponent } from './view/category-detail/category-detail.component';
-import {LoginComponent} from './login/login.component';
-import {SignUpComponent} from './sign-up/sign-up.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { FormsComponent } from '../forms/forms.component';
+import { ButtonsComponent } from '../buttons/buttons.component';
+import { TablesComponent } from '../tables/tables.component';
+import { IconsComponent } from '../icons/icons.component';
+import { TypographyComponent } from '../typography/typography.component';
+import { AlertsComponent } from '../alerts/alerts.component';
+import { AccordionsComponent } from '../accordions/accordions.component';
+import { BadgesComponent } from '../badges/badges.component';
+import { ProgressbarComponent } from '../progressbar/progressbar.component';
+import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+import { TooltipsComponent } from '../tooltips/tooltips.component';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { TabsComponent } from '../tabs/tabs.component';
+import { CategoryComponent } from '../view/category/category.component';
+import { CategoryDetailComponent } from '../view/category-detail/category-detail.component';
+import { LoginComponent } from '../login/login.component';
+import { SignUpComponent } from '../sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/category', pathMatch: 'full' },
@@ -46,7 +46,7 @@ const routes: Routes = [
       { path: '', component: CategoryComponent },
       { path: ':id', component: CategoryDetailComponent },
     ]
-   },
+  },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
 ];
