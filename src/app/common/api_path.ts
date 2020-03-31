64ba997a20562d88aa059843b451ed546b7aabbe
huphc1023 <phuoclh@vnext.com.vn>
2020-03-31 15:50:53 +0700

    detail

--- src/app/common/api_path.ts
+++ src/app/common/api_path.ts
@@ -9,4 +9,6 @@ export class PathAPI {
     public static GET_TIKI_PRODUCT = ApplicationProperties.PATH_API.concat('api/v1/ProductManager/GetTikiProduct');
     public static GET_SENDO_PRODUCT = ApplicationProperties.PATH_API.concat('api/v1/ProductManager/GetSendoProduct');
     public static GET_SHOPEE_PRODUCT_DETAIL = ApplicationProperties.PATH_API.concat('api/v1/ProductManager/GetShopeeProductDetail');
+    public static GET_SENDO_PRODUCT_DETAIL = ApplicationProperties.PATH_API.concat('api/v1/ProductManager/GetSendoProductDetail');
+    public static GET_TIKI_PRODUCT_DETAIL = ApplicationProperties.PATH_API.concat('api/v1/ProductManager/GetTikiProductDetail');
 }
--- src/app/view/product-detail/product-detail.component.ts
+++ src/app/view/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {ProductDetailService} from './product-detail.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,9 +10,94 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
   @Input('data') data: any;
-  constructor() { }
+
+  constructor(
+    private proSer: ProductDetailService,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  isLoading = false;
+  product: any = {
+    name: '',
+    image: '',
+    price: '',
+    url: '',
+    date: '',
+    description: '',
+    thongke: {
+      vitri: '',
+      daban: '',
+      daban30: '',
+      gia: '',
+      giamax: '',
+      giamin: '',
+      likes: '',
+      kho: '',
+      danhgia: '',
+      luotxem: '',
+      diemso: '',
+      isshop: '',
+    },
+    chuyenmuc: [],
+    mausanpham: [
+      {
+        ten: '',
+        ma: '',
+        gia: '',
+        kho: '',
+        daban: ''
+      }
+    ],
+    thuoctinh: [
+      {
+        ten: '',
+        ma: '',
+        giatri: ''
+      }
+    ]
+  };
 
   ngOnInit() {
+    let id;
+    this.route.params.subscribe(params => id = params.id);
+    this.isLoading = true;
+    this.product.name = this.data.name;
+    this.product.price = this.data.price;
+    switch (0) {
+      case id.indexOf('tiki'):
+        this.product.image = this.data.thumbnail_url;
+        this.product.url = 'https:/tiki.vn/' + this.data.url_path;
+        this.product.description = this.data.promotion;
+        this.proSer.getTikiProductDetail(this.data.url_path).subscribe(res => {
+          if (res) {
+
+          }
+          this.isLoading = false;
+        });
+        break;
+      case id.indexOf('shopee'):
+        this.product.price = this.data.price / 100000;
+        this.product.description = this.data.description;
+        this.product.image = 'https://cf.shopee.vn/file/' + this.data.image;
+        this.proSer.getShopeeProductDetail(this.data.itemid, this.data.shopid).subscribe(res => {
+          if (res) {
+          }
+          this.isLoading = false;
+        });
+        break;
+      case id.indexOf('sendo'):
+        this.product.image = this.data.image;
+        this.product.url = 'https:/sendo.vn/' + this.data.cat_path;
+        this.product.description = this.data.description;
+        this.proSer.getSendoProductDetail(this.data.cat_path.substring(0, this.data.cat_path.length - 6)).subscribe((res: any) => {
+          if (res) {
+          }
+          this.isLoading = false;
+        });
+        break;
+    }
+
   }
 
   nothing(event) {
@@ -20,4 +107,5 @@ export class ProductDetailComponent implements OnInit {
   formatCurrency() {
     return new Intl.NumberFormat('en');
   }
+
 }
