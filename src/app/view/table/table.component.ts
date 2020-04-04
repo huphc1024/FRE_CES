6f7d954257068225e16bde91881ca5ffab16af28
anh607567 <anh607567@users.noreply.github.com>
2020-04-05 04:34:46 +0700

    Commit fix table

--- src/app/view/table/table.component.ts
+++ src/app/view/table/table.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter, AfterViewInit, OnChanges } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 import { TableService } from './table.service';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatMenuTrigger } from '@angular/material';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { FormatData } from 'src/app/common/FormatData';
+
+// tslint:disable-next-line:prefer-const
+let global: any;
 
 @Component({
   selector: 'app-table',
@@ -12,11 +15,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @Output() evShowDataDetail = new EventEmitter();
   checkAll = false;
   isLoading = false;
-  rowSelected = {};
+  selected: any[] = [];
   loadMore = 1;
   pagination = {
     page: 1,
@@ -24,85 +26,95 @@ export class TableComponent implements OnInit {
     totalRecords: 0
   };
   dataCopy: any[];
-  dataProduct = [];
   cateId;
   typeTradingFloor = null;
+  coordinate = {
+    x: '0',
+    y: '0'
+  };
+  link;
   mess = {
     1: 'Không có dữ liệu, có thể đề xuất',
     2: 'Đã hết dữ liệu có thể đề xuất',
     3: 'Lỗi API! Không thể get được dữ liệu',
     4: 'Đã sao chép link sản phẩm'
   };
-  displayedColumnsShopee: any[] = [
-    'select',
-    'index',
-    'image',
-    'name',
-    'price',
-    'historical_sold',
-    'ctime',
-    'stock',
-    'discount',
-    'view_count',
-    'liked_count',
-    'cmt_count',
-    'rating_star',
-    'shop_location',
-    'sales'
-  ];
-  displayedColumnsTiKi: any[] = [
-    'select',
-    'index',
-    'thumbnail_url',
-    'name',
-    'price',
-    'order_count',
-    'discount',
-    'discount_rate',
-    'review_count',
-    'favourite_count',
-    'rating_average',
-    'sales'
-  ];
-  displayedColumnsSendo: any[] = [
-    'select',
-    'index',
-    'image',
-    'name',
-    'final_price',
-    'order_count',
-    'original_price',
-    'final_promotion_percent',
-    'shop_name',
-    'counter_view',
-    'counter_like',
-    'total_comment',
-    'percent_star',
-    'sales'
-  ];
-  dataSource = new MatTableDataSource([]);
-  @ViewChild(MatSort, { static: false }) set content(sort: MatSort) {
-    this.dataSource.sort = sort;
-  }
+  colName = [];
+  row = [];
+  rowClone = [];
+  selectSearch = 'all';
+  @ViewChild('contextMenu', { static: false }) menu: ElementRef;
+  @ViewChild(MatMenuTrigger, { static: false }) contextMenu: MatMenuTrigger;
+  @ViewChild('myTable', { static: false }) table: DatatableComponent;
   constructor(
     private tableSer: TableService,
     private acRoute: ActivatedRoute,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    public _formatData: FormatData
   ) { }
-  ngOnInit() {
+  async ngOnInit() {
     this.cateId = this.acRoute.snapshot.params.id;
     switch (0) {
       case this.cateId.indexOf('shopee'):
         this.typeTradingFloor = 0;
+        this.colName = [
+          { name: '#', prop: 'index', width: '55', frozenLeft: true, resizeable: false, sortable: true },
+          { name: 'Ảnh', prop: 'image_dis', sortable: false, frozenLeft: true, resizeable: false, width: '60' },
+          { name: 'Tên sản phẩm', prop: 'name', width: '250' },
+          { name: 'Giá', prop: 'price', width: '100', format: 'currency' },
+          { name: 'SL đã bán', prop: 'historical_sold', width: '100', format: 'number' },
+          { name: 'Thời gian', prop: 'ctime', format: 'date' },
+          { name: 'SL trong kho', prop: 'stock', width: '100', format: 'number' },
+          { name: 'Giảm giá', prop: 'discount', width: '100' },
+          { name: 'Xem', prop: 'view_count', width: '100', format: 'number' },
+          { name: 'Thích', prop: 'liked_count', width: '100', format: 'number' },
+          { name: 'Bình luận', prop: 'cmt_count', width: '100', format: 'number' },
+          { name: 'Đánh giá', prop: 'rating_star', width: '100' },
+          { name: 'Tên cửa hàng', prop: 'shop_location', width: '250' },
+          { name: 'Doanh Thu', prop: 'sales', frozenRight: true, width: '100', resizeable: false, format: 'currency' }
+        ];
         break;
       case this.cateId.indexOf('tiki'):
         this.typeTradingFloor = 1;
+        this.colName = [
+          { name: '#', prop: 'index', width: '55', frozenLeft: true, resizeable: false, sortable: true },
+          { name: 'Ảnh', prop: 'thumbnail_url', sortable: false, frozenLeft: true, resizeable: false, width: '60' },
+          { name: 'Tên sản phẩm', prop: 'name', width: '250' },
+          { name: 'Giá', prop: 'price', width: '100', format: 'currency' },
+          { name: 'SL đã bán', prop: 'order_count', width: '100', format: 'number' },
+          { name: 'Giảm giá', prop: 'discount', width: '100', format: 'currency' },
+          { name: 'Chiết khấu', prop: 'discount_rate', width: '100', format: 'percent' },
+          { name: 'Xem', prop: 'review_count', width: '100', format: 'number' },
+          { name: 'Thích', prop: 'favourite_count', width: '100', format: 'number' },
+          { name: 'Đánh giá', prop: 'rating_average', width: '100' },
+          { name: 'Doanh Thu', prop: 'sales', frozenRight: true, width: '100', resizeable: false, format: 'currency' }
+        ];
         break;
       case this.cateId.indexOf('sendo'):
         this.typeTradingFloor = 2;
+        this.colName = [
+          { name: '#', prop: 'index', width: '55', frozenLeft: true, resizeable: false, sortable: true },
+          { name: 'Ảnh', prop: 'image', sortable: false, frozenLeft: true, resizeable: false, width: '60' },
+          { name: 'Tên sản phẩm', prop: 'name', width: '250' },
+          { name: 'Giá', prop: 'final_price', width: '100', format: 'currency' },
+          { name: 'SL đã bán', prop: 'order_count', width: '100', format: 'number' },
+          { name: 'Giá gốc', prop: 'original_price', width: '100', format: 'currency' },
+          { name: 'Khuyễn mãi', prop: 'final_promotion_percent', width: '100', format: 'percent' },
+          { name: 'Tên cửa hàng', prop: 'shop_name', width: '250', format: 'number' },
+          { name: 'Xem', prop: 'counter_view', width: '100', format: 'number' },
+          { name: 'Thích', prop: 'counter_like', width: '100', format: 'number' },
+          { name: 'Bình luận', prop: 'total_comment', width: '100', format: 'number' },
+          { name: 'Đánh giá', prop: 'percent_star', width: '100', format: 'tofixed2' },
+          { name: 'Doanh Thu', prop: 'sales', frozenRight: true, width: '100', resizeable: false, format: 'currency' }
+        ];
         break;
+      default:
+        this.typeTradingFloor = 999;
+        return;
     }
-    this.onLoadData();
+    setTimeout(async () => {
+      await this.onLoadData();
+    });
   }
   async onLoadData() {
     await this.isLoadData(async () => {
@@ -111,9 +123,9 @@ export class TableComponent implements OnInit {
           await this.tableSer.getShopeeProduct(this.cateId, this.loadMore.toString()).toPromise().then((res: any) => {
             if (res && res.length > 0) {
               this.loadMore += 1;
-              this.dataProduct = res;
+              this.row = this.handlingData(res);
             } else {
-              if (this.dataProduct.length === 0) {
+              if (res.length === 0) {
                 this.typeTradingFloor = 7;
                 return;
               }
@@ -130,9 +142,9 @@ export class TableComponent implements OnInit {
           await this.tableSer.getTikiProduct(this.cateId, this.loadMore.toString()).toPromise().then((res: any) => {
             if (res && res.length > 0) {
               this.loadMore += 1;
-              this.dataProduct = res;
+              this.row = this.handlingData(res);
             } else {
-              if (this.dataProduct.length === 0) {
+              if (res.length === 0) {
                 this.typeTradingFloor = 7;
                 return;
               }
@@ -149,9 +161,9 @@ export class TableComponent implements OnInit {
           await this.tableSer.getSendoProduct(this.cateId, this.loadMore.toString()).toPromise().then((res: any) => {
             if (res && res.length > 0) {
               this.loadMore += 1;
-              this.dataProduct = res;
+              this.row = this.handlingData(res);
             } else {
-              if (this.dataProduct.length === 0) {
+              if (res.length === 0) {
                 this.typeTradingFloor = 7;
                 return;
               }
@@ -165,65 +177,79 @@ export class TableComponent implements OnInit {
           });
           break;
       }
-      this.dataSource.data = this.dataProduct;
-      this.dataSource._updateChangeSubscription();
-      this.dataSource.paginator = this.paginator;
-      this.paginator._changePageSize(this.paginator.pageSize);
-      // this.dataSource.sort = this.sort;
-      this.pagination.totalRecords = this.dataProduct.length;
-      this.pagination.page = 1;
+      this.pagination.totalRecords = this.row.length;
+      this.pagination.page = 0;
+      this.rowClone = [...this.row];
     });
   }
-  onAllSelected(event) {
-    this.checkAll = !this.checkAll;
-    this.dataCopy = [];
-    for (const iterator of this.dataSource.data) {
-      iterator['isChecked'] = this.checkAll;
+  handlingData(arr) {
+    let i = 1;
+    switch (this.typeTradingFloor) {
+      case 0:
+        for (const iterator of arr) {
+          iterator.index = i;
+          iterator.sales = iterator.historical_sold * (iterator.price / 100000);
+          iterator.price = iterator.price / 100000;
+          iterator.ctime = iterator.ctime * 1000;
+          iterator.rating_star = iterator.item_rating.rating_star;
+          iterator.link_redirect = 'https://shopee.vn/product/' + iterator.shopid + '/' + iterator.itemid;
+          iterator.image_dis = 'https://cf.shopee.vn/file/' + iterator.image;
+          i++;
+        }
+        break;
+      case 1:
+        for (const iterator of arr) {
+          iterator.index = i;
+          iterator.sales = iterator.order_count * iterator.price;
+          iterator.price = iterator.price;
+          iterator.link_redirect = 'https://tiki.vn/' + iterator.url_path;
+          i++;
+        }
+        break;
+      case 2:
+        for (const iterator of arr) {
+          iterator.index = i;
+          iterator.sales = iterator.order_count * iterator.final_price;
+          iterator.link_redirect = 'https://www.sendo.vn/' + iterator.cat_path;
+          i++;
+        }
+        break;
     }
-  }
-  async fillerCheckAllData() {
-    for (const iterator of this.dataSource.data) {
-      if (!iterator['isChecked']) {
-        this.checkAll = false;
-        return;
-      }
-    }
-    this.checkAll = true;
-  }
-  onSelectedRow(event, row) {
-    row['isChecked'] = !row['isChecked'];
-    this.fillerCheckAllData();
+    return arr;
   }
   onNothing(event) {
     event.stopPropagation();
   }
-  onPaginatorBT(event) {
-    this.isLoadData(() => {
-      this.pagination.page = event;
-      this.paginator.pageIndex = this.pagination.page - 1;
-      this.paginator._changePageSize(this.paginator.pageSize);
-    });
-  }
-  onPaginator(event) {
-    this.isLoadData(() => {
-      this.pagination.pageSize = event.pageSize;
-      this.pagination.page = event.pageIndex + 1;
-    });
-  }
-  onShowDetail(ele) {
-    this.rowSelected = ele;
-    this.evShowDataDetail.emit(ele);
-  }
   onFilterData(event: Event) {
+    console.log(event);
     this.isLoadData(() => {
-      const filterValue = (event.target as HTMLInputElement).value;
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-      setTimeout(() => {
-        this.pagination.pageSize = this.dataSource.paginator['_pageSize'];
-        this.pagination.totalRecords = this.dataSource.paginator['_length'];
-        this.pagination.page = this.dataSource.paginator['_pageIndex'] + 1;
+      if (this.selectSearch !== 'all') {
+      }
+      if (!event.target['value']) {
+        this.row = [...this.rowClone];
+        return;
+      }
+      if (this.selectSearch === 'all') {
+        this.row = this.rowClone.filter(item => {
+          return item['name'].toLowerCase().includes(event.target['value'].toLowerCase()) ||
+            (item['price'] || 0).toString().toLowerCase().includes(event.target['value'].toLowerCase()) ||
+            (item['sales'] || 0).toString().toLowerCase().includes(event.target['value'].toLowerCase());
+        });
+        return;
+      }
+      this.row = this.rowClone.filter(item => {
+        return (item[this.selectSearch] || 0).toString().toLowerCase().includes(event.target['value'].toLowerCase());
       });
     });
+  }
+  openMenu(event, row) {
+    const coor = event.target.getBoundingClientRect();
+    console.log(this.contextMenu);
+    this.coordinate.x = event.clientX + 'px';
+    this.coordinate.y = (coor.y + 14) + 'px';
+    this.link = row.link_redirect;
+    this.contextMenu.openMenu();
+    return false;
   }
   isLoadData(func) {
     this.isLoading = true;
@@ -232,32 +258,27 @@ export class TableComponent implements OnInit {
       this.isLoading = false;
     });
   }
-  onCopyToClipboard() {
-    const arr = this.dataSource.data.filter((item) => {
-      return item['isChecked'] === true;
+  onShowDetail(event, ele) {
+    console.log(event);
+    console.log(this.table);
+    this.table.element.querySelectorAll('.row-selected').forEach( eleDom => {
+      eleDom.classList.remove('row-selected');
     });
-    let text = [];
-    switch (this.typeTradingFloor) {
-      case 0:
-        text = arr.map((item) => (item.shopid + '/' + item.itemid));
-        for (let index = 0; index < text.length; index++) {
-          text[index] = 'https://shopee.vn/product/' + text[index];
-        }
-        break;
-      case 1:
-        text = arr.map((item) => item.url_path);
-        for (let index = 0; index < text.length; index++) {
-          text[index] = 'https://tiki.vn/' + text[index];
-        }
-        break;
-      case 2:
-        text = arr.map((item) => item.cat_path);
-        for (let index = 0; index < text.length; index++) {
-          text[index] = 'https://www.sendo.vn/' + text[index];
-        }
-        break;
+    global = ele;
+    this.evShowDataDetail.emit(ele);
+  }
+  onSelect({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+  getCellClass(value) {
+    if (global && global.id === value.row.id) {
+      return ' row-selected';
     }
-
+  }
+  onCopyToClipboard() {
+    let text = [];
+    text = this.selected.map((item) => (item.link_redirect));
     const node = document.createElement('textarea');
     const selection = document.getSelection();
     node.textContent = text.join(' ');
@@ -270,25 +291,5 @@ export class TableComponent implements OnInit {
     this._snackBar.open(this.mess[4], 'Undo', {
       duration: 3000
     });
-  }
-  formatCurrency() {
-    return new Intl.NumberFormat('en');
-  }
-  formatNumber(value) {
-    return value && Number(value.toFixed(2));
-  }
-
-  abbreviateNumber(n) {
-    if (n < 1e3) { return n; }
-    if (n >= 1e3 && n < 1e6) { return +(n / 1e3).toFixed(1) + 'k'; }
-    if (n >= 1e6 && n < 1e9) { return +(n / 1e6).toFixed(2) + 'tr'; }
-    if (n >= 1e9 && n < 1e12) { return +(n / 1e9).toFixed(3) + 'tỷ'; }
-    if (n >= 1e12) { return +(n / 1e12).toFixed(1) + 'n tỷ'; }
-  }
-  convertOriginalPrice(value) {
-    if (value) {
-      value = value.replace(/[^0-9]/g, '');
-    }
-    return this.abbreviateNumber(Number(value));
   }
 }
